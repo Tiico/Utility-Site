0c430adde37b89daa61fa8a733b926cad9f20a83
Tiico <ockelberg@gmail.com>
2019-08-21 20:38:45 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={ logo } className="App-logo" alt="logo"/>
-            <h1 className="App-title">React/Redux Express Workbench</h1>
+            <h1 className="App-title">React/Redux Express assaasasas</h1>
           </header>
           <Customers/>
         </div>
