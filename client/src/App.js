d68e20473e11ebfb0e2c8c959b36cdeccb613698
Tiico <ockelberg@gmail.com>
2019-08-21 21:49:10 +0200

    client works to deploy, server remaining

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={ logo } className="App-logo" alt="logo"/>
-            <h1 className="App-title">React/Redux Express IS ALIVE  </h1>
+            <h1 className="App-title">React/Redux Express Workbench </h1>
           </header>
           <Customers/>
         </div>
